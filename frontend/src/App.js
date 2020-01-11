8397cb7cf0ff3ff868b91e1c00ef8202abfc77e6
Leander Lindahl <leanderlindahl@gmail.com>
2020-01-11 13:01:44 +0100

    Add react router dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+
+import AuthPage from './pages/Auth';
+import BookingsPage from './pages/Bookings';
+import EventsPage from './pages/Events';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>It works!</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Redirect from={'/'} to={'/auth'} exact />
+        <Route path="/auth" component={AuthPage} />
+        <Route path="/events" component={EventsPage} />
+        <Route path="/bookings" component={BookingsPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
