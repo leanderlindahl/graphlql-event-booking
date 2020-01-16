d94436a84e6eceb8a799634734c599c4cf6b4400
Leander Lindahl <leanderlindahl@gmail.com>
2020-01-16 13:05:47 +0100

    Add CSS: adjust navbar

--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -11,7 +11,7 @@ const mainNavigation = props => (
       return (
         <header className="main-navigation">
           <div className="main-navigation__logo">
-            <h1>GraphQL Event Booking</h1>
+            <h1>Eventify</h1>
           </div>
           <nav className="main-navigation__items">
             <ul>
