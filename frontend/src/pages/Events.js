fb1487bbed61a8c9f7ef269bca6d4bf47402ff43
Leander Lindahl <leanderlindahl@gmail.com>
2020-01-20 11:18:28 +0100

    eslint-plugin-jsx-a11y, refactor Events.js

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,57 +1,72 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 import Modal from '../components/Modal/Modal';
 import Backdrop from '../components/Backdrop/Backdrop';
 import './Events.css';
 
-class EventsPage extends Component {
-  state = {
-    creating: false
-  };
-  startCreateEventHandler = event => {
+const EventsPage = () => {
+  const [creating, setCreating] = useState(false);
+
+  const startCreateEventHandler = event => {
     event.preventDefault();
-    this.setState({ creating: true });
+    setCreating(true);
   };
 
-  modalConfirmHandler = () => {};
-  modalCancelHandler = () => {
-    this.setState({ creating: false });
+  const modalConfirmHandler = () => {};
+  const modalCancelHandler = () => {
+    setCreating(false);
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        {this.state.creating && <Backdrop />}
-        {this.state.creating && (
-          <Modal
-            title="Add Event"
-            canCancel
-            canConfirm
-            onCancel={this.modalCancelHandler}
-            onConfirm={this.modalConfirmHandler}
-          >
-            <p>Modal Content</p>
-          </Modal>
-        )}
+  return (
+    <>
+      {creating && <Backdrop />}
+      {creating && (
+        <Modal
+          title="Add Event"
+          canCancel
+          canConfirm
+          onCancel={modalCancelHandler}
+          onConfirm={modalConfirmHandler}
+        >
+          <form>
+            <div className="form-control">
+              <label htmlFor="title">Title</label>
+              <input type="text" id="title" name="title" />
+            </div>
+            <div className="form-control">
+              <label htmlFor="price">Price</label>
+              <input type="number" id="price" name="price" />
+            </div>
+            <div className="form-control">
+              <label htmlFor="date">Date</label>
+              <input type="date" id="date" name="date" />
+            </div>
+            <div className="form-control">
+              <label htmlFor="description">Description</label>
+              <textarea id="description" name="description" rows="4" />
+            </div>
+          </form>
+        </Modal>
+      )}
 
-        <article className="events">
-          <header>
-            <h1>Share your own Events!</h1>
-          </header>
-          <main>
-            <form className="events-control">
-              <button
-                className="button button-primary"
-                onClick={this.startCreateEventHandler}
-              >
-                Create Event
-              </button>
-            </form>
-          </main>
-        </article>
-      </React.Fragment>
-    );
-  }
-}
+      <article className="events">
+        <header>
+          <h1>Share your own Events!</h1>
+        </header>
+        <main>
+          <form className="events-control">
+            <button
+              type="button"
+              className="button button-primary"
+              onClick={startCreateEventHandler}
+            >
+              Create Event
+            </button>
+          </form>
+        </main>
+      </article>
+    </>
+  );
+};
 
 export default EventsPage;
