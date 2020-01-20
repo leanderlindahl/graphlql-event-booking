55994f2112fac11d9594fbf24f2616920e8eec38
Leander Lindahl <leanderlindahl@gmail.com>
2020-01-20 15:40:33 +0100

    Add 'add event' and 'event listing'

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,22 +1,145 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 
 import Modal from '../components/Modal/Modal';
 import Backdrop from '../components/Backdrop/Backdrop';
+import AuthContext from '../context/auth-context';
 import './Events.css';
 
 const EventsPage = () => {
+  const context = useContext(AuthContext);
+  const [events, setEvents] = useState([]);
+
   const [creating, setCreating] = useState(false);
+
+  const titleElRef = React.createRef();
+  const priceElRef = React.createRef();
+  const dateElRef = React.createRef();
+  const descriptionElRef = React.createRef();
+
+  const fetchEvents = () => {
+    const requestBody = {
+      query: `
+          query {
+            events {
+              _id
+              title
+              description
+              price
+              date
+              creator {
+                _id
+                email
+              }
+            }
+          }
+        `
+    };
+
+    fetch('http://localhost:8001/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+        const retrievedEvents = resData.data.events;
+        setEvents(retrievedEvents);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
   const startCreateEventHandler = event => {
     event.preventDefault();
     setCreating(true);
   };
 
-  const modalConfirmHandler = () => {};
+  const modalConfirmHandler = () => {
+    setCreating(false);
+    const title = titleElRef.current.value;
+    const price = +priceElRef.current.value;
+    const date = dateElRef.current.value;
+    const description = descriptionElRef.current.value;
+
+    if (
+      title.trim().length === 0 ||
+      price.length < 0 ||
+      date.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      return;
+    }
+    const event = {
+      title,
+      price,
+      date,
+      description
+    };
+
+    console.log('modalConfirm event', event);
+
+    const requestBody = {
+      query: `
+          mutation {
+            createEvent(eventInput: {title: "${title}", description: "${description}", price: ${price}, date: "${date}"}) {
+              _id
+              title
+              description
+              price
+              date
+              creator {
+                _id
+                email
+              }
+            }
+          }
+        `
+    };
+
+    fetch('http://localhost:8001/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${context.token}`
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+        fetchEvents();
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
   const modalCancelHandler = () => {
     setCreating(false);
   };
 
+  const eventsList = events.map(event => (
+    <li key={event._id} className="events__list-item">
+      {event.title}
+    </li>
+  ));
   return (
     <>
       {creating && <Backdrop />}
@@ -31,19 +154,29 @@ const EventsPage = () => {
           <form>
             <div className="form-control">
               <label htmlFor="title">Title</label>
-              <input type="text" id="title" name="title" />
+              <input type="text" id="title" name="title" ref={titleElRef} />
             </div>
             <div className="form-control">
               <label htmlFor="price">Price</label>
-              <input type="number" id="price" name="price" />
+              <input type="number" id="price" name="price" ref={priceElRef} />
             </div>
             <div className="form-control">
               <label htmlFor="date">Date</label>
-              <input type="date" id="date" name="date" />
+              <input
+                type="datetime-local"
+                id="date"
+                name="date"
+                ref={dateElRef}
+              />
             </div>
             <div className="form-control">
               <label htmlFor="description">Description</label>
-              <textarea id="description" name="description" rows="4" />
+              <textarea
+                id="description"
+                name="description"
+                ref={descriptionElRef}
+                rows="4"
+              />
             </div>
           </form>
         </Modal>
@@ -54,15 +187,22 @@ const EventsPage = () => {
           <h1>Share your own Events!</h1>
         </header>
         <main>
-          <form className="events-control">
-            <button
-              type="button"
-              className="button button-primary"
-              onClick={startCreateEventHandler}
-            >
-              Create Event
-            </button>
-          </form>
+          <section>
+            {context.token && (
+              <form className="events-control">
+                <button
+                  type="button"
+                  className="button button-primary"
+                  onClick={startCreateEventHandler}
+                >
+                  Create Event
+                </button>
+              </form>
+            )}
+          </section>
+          <section>
+            <ul className="events__list">{eventsList}</ul>
+          </section>
         </main>
       </article>
     </>
